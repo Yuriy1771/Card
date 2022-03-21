20f9bda10997d01ba67d397a41ce8eb8bb91b69c
Yuriy <dementev.yuriy@list.ru>
2022-03-21 18:20:12 +0300

    добваил validation на select month,year и input cw

--- card.js
+++ card.js
@@ -5,6 +5,9 @@ let error = document.querySelector('.error');
 
 let inputCardNumber;
 let inputCardName;
+let selectMonth;
+let selectYear;
+let selectCW;
 
 initLets();
 
@@ -14,21 +17,30 @@ sendBtn.onclick = () => {
         
     } else {
         error.innerHTML = resultValid;
-        inputCardNumber.style.border = "1px solid red";
-        inputCardName.style.border = '1px solid red';
+        // inputCardNumber.style.border = "1px solid red";
+        // inputCardName.style.border = '1px solid red';
     }
 }
 
 function initLets() {
     inputCardNumber = document.querySelector('#cardNumber');
     inputCardName = document.querySelector('#cardName');
-};
+    selectYear = document.querySelector('#year');
+    selectMonth = document.querySelector('#month');
+    selectCW = document.querySelector('#cw');
+}
 
 function validateSendBtn() {
     if(inputCardNumber.value === '') {
         return 'You are not enter <strong>card number</strong>';
     } if(inputCardName.value === '') {
         return 'You are not enter <strong>card name</strong>';
+    }if(selectMonth.value === 'Month') {
+        return 'You are not chose <strong>month</strong>';
+    } if(selectYear.value === 'Year') {
+        return 'You are not chose <strong>year</strong>';
+    } if(selectCW.value === '') {
+        return 'You are not chose <strong>CW</strong>';
     }
     return '';
 }
