8261a00244ecda57bf237e5021b421a3760dc8fd
Yuriy <dementev.yuriy@list.ru>
2022-03-22 16:18:42 +0300

    добавил маску на номер карты на input CardNumber

--- card.js
+++ card.js
@@ -11,6 +11,10 @@ let selectCW;
 
 initLets();
 
+// при нажатии на send запускается анонимная функция в которой объявляется переменная resultValid
+// и к ней присваивается значение функции validateSendBtn, после запускается условие if в котором
+// resultValid проверяется на валидацию, если resultValid === '' то trur, иначе вывести ошибку через
+// innerHTML
 sendBtn.onclick = () => {
     let resultValid = validateSendBtn();
     if(resultValid === '') {
@@ -22,14 +26,17 @@ sendBtn.onclick = () => {
     }
 }
 
+// данная функция инициализирует переменные
 function initLets() {
     inputCardNumber = document.querySelector('#cardNumber');
     inputCardName = document.querySelector('#cardName');
     selectYear = document.querySelector('#year');
     selectMonth = document.querySelector('#month');
-    selectCW = document.querySelector('#cw');
+    inputCW = document.querySelector('#cw');
 }
 
+// данная функция вызывает условия if. Если переменная равна пустоте то, выведи return,если переменная
+// не равна пустоте то верни знвчение
 function validateSendBtn() {
     if(inputCardNumber.value === '') {
         return 'You are not enter <strong>card number</strong>';
@@ -39,8 +46,42 @@ function validateSendBtn() {
         return 'You are not chose <strong>month</strong>';
     } if(selectYear.value === 'Year') {
         return 'You are not chose <strong>year</strong>';
-    } if(selectCW.value === '') {
+    } if(inputCW.value === '') {
         return 'You are not chose <strong>CW</strong>';
     }
     return '';
 }
+
+
+
+window.addEventListener("DOMContentLoaded", function() {
+function setCursorPosition(pos, elem) {
+    elem.focus();
+    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+    else if (elem.createTextRange) {
+        let range = elem.createTextRange();
+        range.collapse(true);
+        range.moveEnd("character", pos);
+        range.moveStart("character", pos);
+        range.select()
+    }
+}
+
+function mask(event) {
+    let matrix = this.defaultValue,
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, "");
+        def.length >= val.length && (val = def);
+    matrix = matrix.replace(/[*\d]/g, function(a) {
+        return val.charAt(i++) || "*"
+    });
+    this.value = matrix;
+    i = matrix.lastIndexOf(val.substr(-1));
+    i < matrix.length && matrix != this.defaultValue ? i++ : i = matrix.indexOf("*");
+    setCursorPosition(i, this)
+}
+
+    let input = document.querySelector("input");
+    input.addEventListener("input", mask, false)
+});
