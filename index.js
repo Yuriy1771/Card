9399b8e28251ffe85149d618d7d1931d4d66eb6b
Yuriy <dementev.yuriy@list.ru>
2022-03-23 17:47:21 +0300

    добавил модальное окно и стилизовал его, сделал три возможности его закрыть на кнопку close и зону window

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ btnSend.onclick = () => {
     initLets();
     let resultValid = validateBtnSend();
     if(resultValid === '') {
-
+        modalOpen();
+        // document.location.href = 'file:///C:/Users/gamep/Desktop/Js_%D0%BF%D1%80%D0%B0%D0%BA%D1%82%D0%B8%D0%BA%D0%B8/card/card.html';
     } else {
         errorValid.innerHTML = resultValid;
     }
@@ -25,7 +26,27 @@ function validateBtnSend() {
     } if (inputAge === '') {
         return 'Please, enter your <strong>age</strong>';
     } if(inputAge < 16) {
-        return 'You are still small';
+        return 'You are <strong>still small</strong>';
     }
     return '';
+}
+
+// модальное окно при нажати на кнопку Send
+
+const btnOk = document.querySelector('.btnOk');
+const close = document.querySelector('.close');
+const modalWindow = document.querySelector('#myModal');
+
+function modalOpen() {
+    modalWindow.style.display = 'block';
+}
+
+close.onclick = () => {
+    modalWindow.style.display = 'none';
+}
+
+window.onclick = (event) => {
+    if(event.target === modalWindow) {
+        modalWindow.style.display = 'none';
+    }
 }
